0146179c743cfaca1680a8e6fd2753385b417959
Nazarov Kanat <kanatnazarov.dev@gmail.com>
2026-03-09 15:31:15 -0400

    added new link

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,41 +1,74 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const { firstName, lastName, email, phone, lang } = body;
 
-    const cleanPhone = `+1${phone.replace(/\D/g, '').slice(-10)}`;
-    const newLink = 'https://tds-leads.vercel.app/api/leads/nightlase_form'
-    const old = 'https://services.leadconnectorhq.com/contacts/upsert'
-    const response = await fetch(old, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${process.env.GHL_PRIVATE_TOKEN}`,
-        'Content-Type': 'application/json',
-        'Version': '2021-07-28'
+    const cleanPhone = `+1${phone.replace(/\D/g, "").slice(-10)}`;
+
+    const ghlRequest = fetch(
+      "https://services.leadconnectorhq.com/contacts/upsert",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.GHL_PRIVATE_TOKEN}`,
+          "Content-Type": "application/json",
+          Version: "2021-07-28",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          phone: cleanPhone,
+          locationId: process.env.GHL_LOCATION_ID,
+          tags: [
+            "NightLase Landing Page",
+            lang === "es" ? "Spanish" : "English",
+          ],
+          source: "Nightlase",
+        }),
       },
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        phone: cleanPhone,
-        locationId: process.env.GHL_LOCATION_ID,
-        tags: ['NightLase Landing Page', lang === 'es' ? 'Spanish' : 'English'],
-        source: 'Nightlase'
-      }),
-    });
+    );
+    const tdsRequest = fetch(
+      "https://tds-leads.vercel.app/api/nightlase-leads",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          data: [
+            {
+              first_name: firstName,
+              last_name: lastName,
+              phone_number: cleanPhone,
+            },
+          ],
+        }),
+      },
+    );
 
-    const data = await response.json();
-
-    if (!response.ok) {
-      console.log(response)
-      return NextResponse.json({ error: data.message || "GHL API Error" }, { status: response.status });
+    const [ghlResponse, tdsResponse] = await Promise.all([tdsRequest, ghlRequest]);
+    // 3. Check for errors
+    if (!ghlResponse.ok) {
+      const ghlData = await ghlResponse.json();
+      return NextResponse.json(
+        { error: ghlData.message || "GHL API Error" },
+        { status: ghlResponse.status },
+      );
     }
 
+    if (!tdsResponse.ok) {
+      return NextResponse.json(
+        { error: "TDS API Error" },
+        { status: tdsResponse.status },
+      );
+    }
     return NextResponse.json({ success: true });
   } catch (error) {
     console.error("API Route Error:", error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
   }
 }
